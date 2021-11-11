617c05fd392ed96b87ab17437e229734acdcc4a0
Petar Peshev <petar.peshev@leanplum.com>
2021-11-11 11:42:36 +0200

    add staging deploy script

---
+++ staging-deploy.sh
@@ -0,0 +1,7 @@
+sudo snap install yq
+echo $PR_NUMBER
+export FULL_PR_NUMBER="pr-$PR_NUMBER"
+yq eval -i '.app.versions += [env(FULL_PR_NUMBER)'] superhero-ui/values-staging.yaml
+yq eval -i '.app.ingress.hosts += [{"host": "'${FULL_PR_NUMBER}'.stg.aepps.com", "paths": { "path": "/"}}]' superhero-ui/values-staging.yaml
+yq eval -i '.. style="double"' superhero-ui/values-staging.yaml
+cat superhero-ui/values-staging.yaml
