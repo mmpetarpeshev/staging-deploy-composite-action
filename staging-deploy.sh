3c4d0401d31e02c55b5fa384933192385c003199
Petar Peshev <petar.peshev@leanplum.com>
2021-11-11 22:00:17 +0200

    dasda

--- staging-deploy.sh
+++ staging-deploy.sh
@@ -2,9 +2,3 @@
 set -x
 PR_NUMBER=$1
 echo $PR_NUMBER
-export FULL_PR_NUMBER="pr-$PR_NUMBER"
-sudo snap install yq
-yq eval -i '.app.versions += [env(FULL_PR_NUMBER)'] superhero-ui/values-staging.yaml
-yq eval -i '.app.ingress.hosts += [{"host": "'${FULL_PR_NUMBER}'.stg.aepps.com", "paths": { "path": "/"}}]' superhero-ui/values-staging.yaml
-yq eval -i '.. style="double"' superhero-ui/values-staging.yaml
-cat superhero-ui/values-staging.yaml
